38d09ade3e85a1f8f1e2951b6009d18e9b783148
marwairimu <92425844+marwairimu@users.noreply.github.com>
2021-12-03 10:37:39 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+/*
+    project: library system
+    Author:  mary wairimu
+    Date:    oct, 2021
+    language:C99
+    Compiler:GCC
+    License: MIT
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("tcounty library\n");
+    printf("welcome mary\n");
+    printf("what would you like to do\n");
+    printf("1.add a new user\n");
+    printf("2.get a new book\n");
+    printf("3.borrow a book\n");
+    printf("4.return a book\n");
+    printf("5.write reports\n");
+    printf("6.acess the internet\n");
+    printf("7.do independent work\n");
+    printf("8.use computer\n");
+    printf("9.use equipment and research materials\n");
+    printf("10.host special events\n");
+    printf("11.tutoring and testing\n");
+
+    return 0;
+}
